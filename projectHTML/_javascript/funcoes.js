77529a5e90896b3f93b372a6a018b4761be36710
Talita München <tt.munchen@gmail.com>
2016-07-18 14:47:12 -0300

    add_project_google_glas_test

---
+++ projectHTML/_javascript/funcoes.js
@@ -0,0 +1,8 @@
+function mudaFoto (foto){
+	document.getElementById("icone").src = foto; /*mudar as fotos dos elementos do menu*/
+}
+
+/*
+colcoar quantas funções QUISER, separa para deixar mais organizado
+formato javascript
+*/
